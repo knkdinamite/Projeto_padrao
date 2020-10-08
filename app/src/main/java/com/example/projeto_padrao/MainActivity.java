57cd081fd9b9092c530c149b617bbb0f550bcbd1
Sala 19A <sala19a@ceines.com.br>
2020-10-07 21:36:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projeto_padrao/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.projeto_padrao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/projeto_padrao/NovaActivity.java
@@ -0,0 +1,19 @@
+package com.example.projeto_padrao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+
+public class NovaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.minha_tela);
+
+
+
+
+    }
+}
