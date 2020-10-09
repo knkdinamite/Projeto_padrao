51032255919306bcf9fca7ce9247f02e203496f3
knkdinamite <knkdinamite@hotmail.com>
2020-10-08 21:47:29 -0300

    third

--- app/src/main/java/com/example/projeto_padrao/NovaActivity.java
+++ app/src/main/java/com/example/projeto_padrao/NovaActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.service.autofill.OnClickAction;
+import android.util.Log;
 
 public class NovaActivity extends AppCompatActivity {
 
@@ -11,9 +12,33 @@ public class NovaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.minha_tela);
-
-
-
+    Log.d("Ciclo_vivido", "onCreate - a novaActivity iniciou");
 
     }
+    @Override
+    protected void onStart(){
+        Log.d( "Ciclo_vivido", "onStart-Comecando");
+        super.onStart();
+    }
+    @Override
+    protected void onResume(){
+        Log.d("Ciclo_vivido","onResume - Estado de interaçao com a tela");
+        super.onResume();
+    }
+    @Override
+    protected  void onPause(){
+        Log.d( "Ciclo_vivido", "onPause - iniciou o termino da actuvity");
+        super.onPause();
+    }
+    @Override
+    protected  void onStop(){
+        Log.d( "Ciclo_vivido", "onStop - fechou a aplicaçao(sem atividade visivel)");
+        super.onStop();
+    }
+    @Override
+    protected  void onDestroy(){
+        Log.d( "Ciclo_vivido", "onDestroy - activity completamente destruida");
+        super.onDestroy();
+    }
+
 }
